f97d744fb0b7f17a5c02d00f77d08e8655456994
cuizihang1145 <340313894@qq.com>
2026-08-26 20:11:37 +0800

    气笑了

--- api/friends.js
+++ api/friends.js
@@ -1,14 +1,6 @@
-// 这个文件的唯一作用：让 Vercel 构建日志显示它被识别为 Serverless Function
-export default function handler(req) {
-    return new Response(JSON.stringify({
+export default function handler(req, res) {
+    res.status(200).json({
         status: 'ok',
-        timestamp: Date.now(),
-        message: '如果看到这条消息，说明函数已成功部署并执行'
-    }), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-        },
+        message: '函数运行正常，可以正常访问了'
     });
 }
